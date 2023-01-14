0113e28b8c4f3c74a0eb6cced0fc91dbf059962f
madan <madannikalje44@gmail.com>
2023-01-14 16:52:16 +0530

    chore: remove unused file

--- src/strategies/local.strategy.ts
+++
@@ -1,7 +0,0 @@
-import passport from 'passport';
-import { Strategy } from 'passport-local'
-
-passport.use(new Strategy({ usernameField: "email" }, (email: string, password: string, done) => {
-    console.log( email, password )
-    done(null,true)
-}));
