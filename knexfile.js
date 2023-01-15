c8697fba809f1a8ba8e98ef163975b7ce672c2a7
madan <madannikalje44@gmail.com>
2023-01-15 19:38:02 +0530

    chore: fix knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,15 @@
 module.exports = {
-    development: {
-            client: 'pg',
-            connection: {
-            host: "database-1.clypw1lkrb0q.ap-south-1.rds.amazonaws.com",
-            user: "postgres",
-            password: "madan12!",
-            database: "test_db",
-        },
-        migrations: {
-        directory: './src/database/migrations',
-        tableName: 'knex_migrations'
-        },
-    }
-}
+  development: {
+    client: "pg",
+    connection: {
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+    },
+    migrations: {
+      directory: "./src/database/migrations",
+      tableName: "knex_migrations",
+    },
+  },
+};
